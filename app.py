f76751f0cfab6a902b37207731aace78bba8f493
ipsa0926 <132085193+ipsa0926@users.noreply.github.com>
2023-04-28 23:19:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from flask import Flask
+    
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    date = datetime.now()
+    return str(date)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000)
